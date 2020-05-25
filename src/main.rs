3c8a58c24994dab7561d893478e3e9e95196f7be
Alex Hunt <alex.s.hunt7@gmail.com>
2020-05-24 20:33:17 -0700

    works with state now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-//use std::fs::File;
-//use std::io;
-//use std::sync::Mutex;
+use std::fs::File;
 
 use futures_util::TryStreamExt;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use serde::Deserialize;
 use serde::Serialize;
-use std::convert::Infallible;
+use serde_json;
 use std::net::SocketAddr;
+use std::sync::{Arc, RwLock};
 
 #[derive(Serialize, Deserialize)]
 struct NetworkInterface {
@@ -27,11 +26,6 @@ struct Machine {
 }
 
 #[derive(Serialize, Deserialize)]
-struct AppState {
-    machines: Vec<Machine>,
-}
-
-#[derive(Serialize, Deserialize)]
 enum Status {
     Idle,
     InPXEBoot,
@@ -43,88 +37,40 @@ struct MyObj {
     id: u32,
 }
 
-//#[get("/{id}/{name}/index.html")]
-//async fn index(info: web::Path<(u32, String)>) -> HttpResponse {
-//    HttpResponse::Ok().json(MyObj {
-//        name: info.1.clone(),
-//        id: info.0,
-//    })
-//}
-//
-//#[get("/v1/list")]
-//async fn list_machines(
-//    data: web::Data<Mutex<AppState>>,
-//) -> Result<HttpResponse, std::sync::PoisonError<AppState>> {
-//    Ok(HttpResponse::Ok().json(&data.lock()?.machines))
-//}
-
-//#[derive(Debug)]
-//enum Error {
-//    YamlError(serde_yaml::Error),
-//    IoError(io::Error),
-//}
-//
-//impl fmt::Display for Error {
-//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//        match *self {
-//            YamlError => f.write_str("Error parsing yaml"),
-//            IoError => f.write_str("Error with IO"),
-//        }
-//    }
-//}
-//
-//impl std::error::Error for Error {
-//    fn description(&self) -> &str {
-//        match *self {
-//            YamlError => "Error parsing yaml",
-//            IoError => "Error with IO",
-//        }
-//    }
-//    //fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-//
-//    //}
-//}
-//
-//impl From<serde_yaml::Error> for Error {
-//    fn from(e: serde_yaml::Error) -> Self {
-//        Error::YamlError(e)
-//    }
-//}
-//
-//impl From<io::Error> for Error {
-//    fn from(e: io::Error) -> Self {
-//        Error::IoError(e)
-//    }
-//}
-
-//#[actix_rt::main]
-//async fn main() -> std::io::Result<()> {
-//    HttpServer::new(move || {
-//        //let machines = Vec::<Machine>::new();
-//        let f = File::open("machines.yml").unwrap();
-//        let machines: Vec<Machine> = serde_yaml::from_reader(f).unwrap();
-//        let app_state = web::Data::new(Mutex::new(AppState { machines: machines }));
-//        App::new()
-//            .wrap(middleware::Logger::default())
-//            .data(web::JsonConfig::default().limit(4096))
-//            .app_data(app_state)
-//            .service(list_machines)
-//            .service(index)
-//    })
-//    .bind("[::1]:8080")?
-//    .run()
-//    .await
-//}
-
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Hello, World!".into()))
+#[derive(Serialize, Deserialize)]
+struct State {
+    machines: Vec<Machine>,
 }
 
-async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+fn load_machines() -> Result<State, Box<dyn std::error::Error>> {
+    let f = File::open("machines.yml")?;
+    let machines: Vec<Machine> = serde_yaml::from_reader(f)?;
+    Ok(State { machines: machines })
+}
+
+async fn list_machines(state: Arc<RwLock<State>>) -> Result<Response<Body>, hyper::Error> {
+    let state = &(*state.read().unwrap());
+    let json = serde_json::to_string_pretty(state).unwrap();
+    Ok(Response::new(json.into()))
+}
+
+async fn echo(
+    state: Arc<RwLock<State>>,
+    req: Request<Body>,
+) -> Result<Response<Body>, hyper::Error> {
+    // TODO better router?
+    // maybe https://github.com/kardeiz/reset-router
+    // maybe https://github.com/lambdax-x/rouste
+
+    // /v1/list
+    // /v1/boot/{mac}
+    // /v1/triggerBoot/mac/{mac}/payload/{payload}
+
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(Body::from(
             "Try POSTing data to /echo such as: `curl localhost:3000/echo -XPOST -d 'hello world'`",
         ))),
+        (&Method::GET, "/v1/list") => list_machines(state).await,
 
         (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
 
@@ -153,16 +99,24 @@ async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    // TODO load yaml?
+    let state = Arc::new(RwLock::new(load_machines()?));
 
-    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+    let service = make_service_fn(move |_| {
+        let state = state.clone();
+        async move {
+            Ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
+                let state = state.clone();
+                echo(state, req)
+            }))
+        }
+    });
 
     let server = Server::bind(&addr).serve(service);
 
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    server.await?;
+    Ok(())
 }
